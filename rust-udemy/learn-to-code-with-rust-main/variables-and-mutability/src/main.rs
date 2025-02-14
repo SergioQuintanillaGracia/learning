58f1cb564d996851d36bb0ef437e8c3c5847982e
SergioQuintanilla <sergioquintanillagracia@gmail.com>
2025-02-14 15:09:09 +0100

    Added run task

--- rust-udemy/learn-to-code-with-rust-main/variables-and-mutability/src/main.rs
+++ rust-udemy/learn-to-code-with-rust-main/variables-and-mutability/src/main.rs
@@ -1,1 +1,12 @@
-fn main() {}
+fn main() {
+    let apples = 50;
+    let oranges = 14 + 6;
+    let fruits = apples + oranges;
+
+    println!("This year, my garden has {0} apples", apples);
+    println!("This year, my garden has {apples} apples");
+    println!(
+        "My garden has {0} apples and {1} fruits. Yes, I have {0} apples",
+        apples, fruits
+    );
+}
