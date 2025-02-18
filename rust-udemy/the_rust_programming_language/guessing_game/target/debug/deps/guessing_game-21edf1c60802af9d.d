ae71101754c8287d7c04c7345aa3d46121f2a952
Sergio Quintanilla <sergioquintanillagracia@gmail.com>
2025-02-18 13:40:19 +0100

    Started following guessing game guide (rust book)

---
+++ rust-udemy/the_rust_programming_language/guessing_game/target/debug/deps/guessing_game-21edf1c60802af9d.d
@@ -0,0 +1,5 @@
+/home/sergio/Documents/GitHub/learning/rust-udemy/the_rust_programming_language/guessing_game/target/debug/deps/libguessing_game-21edf1c60802af9d.rmeta: src/main.rs
+
+/home/sergio/Documents/GitHub/learning/rust-udemy/the_rust_programming_language/guessing_game/target/debug/deps/guessing_game-21edf1c60802af9d.d: src/main.rs
+
+src/main.rs:
