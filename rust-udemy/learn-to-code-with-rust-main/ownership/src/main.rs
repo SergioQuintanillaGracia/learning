60c10c7a52c77aea377a65f254a5eee63994de51
SergioQuintanilla <sergioquintanillagracia@gmail.com>
2025-02-19 22:15:40 +0100

    Section 6 progress

--- rust-udemy/learn-to-code-with-rust-main/ownership/src/main.rs
+++ rust-udemy/learn-to-code-with-rust-main/ownership/src/main.rs
@@ -1,1 +1,28 @@
-fn main() {}
+fn main() {
+    let time = 2025;
+    let year = time;
+
+    let text = String::new();
+    let text2 = String::from("I am a string");
+    println!("{}", text2);
+
+    let mut name = String::from("Boris");
+    println!("{name}");
+    name.push_str(" Pask");
+    println!("{name}");
+
+    let person = String::from("Boris");
+    let genius = person;
+
+    let person = String::from("Boris");
+    let genius = person.clone();
+    println!("{person}, {genius}");
+
+    let my_stack_value = 2;
+    let my_integer_reference = &my_stack_value;
+
+    let my_heap_value = String::from("Toyota");
+    let my_heap_reference = &my_heap_value;
+    println!("{}", *my_heap_reference);
+    println!("{}", my_heap_reference);
+}
